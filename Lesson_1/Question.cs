28c35f6cf5394de05cc5fd84ec9b58a0b115a21d
BezdelMA <BezdelMA@mail.ru>
2019-07-28 10:19:39 +0300

    Выполнил все задания, постарался их усложнить для дополнительной тренировки

---
+++ Lesson_1/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public class Question
+    {
+        public static List<string> textList = new List<string>();
+        public static string Ques (string text)
+        {
+            Console.Write("{0}: ", text);
+            textList.Add(text);
+            return Console.ReadLine();
+        }
+
+        internal static void WriteQues(string i, int txt)
+        {
+                Console.Write("{0} {1}; ", textList[txt], i);
+        }
+    }
+}
---
+++ _3/Point.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public Random rnd = new Random();
+
+        public Point ()
+        {
+            GetRandom();
+        }
+
+        public void GetRandom ()
+        {
+            x = rnd.Next(1, 100);
+            y = rnd.Next(1, 30);
+            Thread.Sleep(15);
+        }
+
+        internal void Show(int _x, int _y, int i)
+        {
+            Console.WriteLine("Координаты точки № {0}: {1}, {2}", i, _x, _y);
+        }
+
+        internal static void Distance(Point p1, Point p2)
+        {
+            double distance = Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+            Console.WriteLine("Расстояние между двумя произвольными точками равна: {0:F2}", distance);
+        }
+    }
+}
