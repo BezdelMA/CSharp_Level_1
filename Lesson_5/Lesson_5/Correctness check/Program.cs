17d224cdb804b0ce070043ede71f7db29afec2ab
BezdelMA <BezdelMA@mail.ru>
2019-08-19 07:20:39 +0300

    Почти все сделал

---
+++ Lesson_5/Lesson_5/Correctness check/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Программа на проверку введенного логина. Все условия соблюдены, программа работает
+//В том числе, производится проверка с использованием регулярных выражений
+
+namespace Correctness_check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Внимание!!! Логин должен содержать от 2 до 10 символов\nсодержать буквы только латинского алфавита и цифры,\nпри этом цифра не может быть первой\n");
+            bool flag = false;
+            while (!flag)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\nВведите логин: ");
+                Login login = new Login(Console.ReadLine());
+                flag = login.Check();
+                login.CheckRegular();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_5/Lesson_5/Student/Student.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        string surname;
+        string name;
+        string point;
+        
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Point
+        {
+            get
+            {
+                return point;
+            }
+
+            set
+            {
+                point = value;
+            }
+        }
+
+        public string Print
+        {
+            get
+            {
+                string str = "";
+                foreach (var i in point)
+                    str += i + " ";
+                return str;
+            }
+        }
+
+        internal string Write()
+        {
+            return surname + " " + name + " " + point;
+        }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string surname, string name, string point)
+        {
+            Surname = surname;
+            Name = name;
+            Point = point;
+        }
+
+        public static Student EnterStudent()
+        {
+            Console.Write("Введите фамилию студента: ");
+            string surname = CheckString(20, "Фамилия");
+            Console.Write("Введите имя студента: ");
+            string name = CheckString(15, "Имя");
+            Console.Write("Через пробел введите 3 оценки студента: ");
+            string point = CheckPoint();
+
+            return new Student(surname, name, point);
+        }
+
+        public static string CheckString(int p, string temp)
+        {
+            while (true)
+            {
+                try
+                {
+                    string txt = Console.ReadLine();
+                    if (txt.Length > p) throw new Exception("Повторите ввод: ");
+                    return txt;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0} содержит не более {1} символов. {2}", temp, p, ex.Message);
+                    continue;
+                }
+            }
+        }
+
+        public static string CheckPoint()
+        {
+            while (true)
+            {
+                try
+                {
+                    string txt = Console.ReadLine();
+                    string[] temp = txt.Split(' ');
+                    txt = "";
+                    for (int i = 0; i < 3; i++)
+                    {
+                        if (!Int32.TryParse(temp[i], out int x) || temp[i].Length > 1 || x > 5 || x < 1) throw new Exception("Оценки должны содержать только целые цифры от 1 до 5. Повторите ввод: ");
+                        else
+                        {
+                            txt += temp[i] + " ";
+                        }
+                    }
+                    return txt;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+            }
+
+        }
+    }
+}
